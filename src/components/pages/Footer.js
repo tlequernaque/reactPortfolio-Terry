43304111e69f166468a6e2c6f1019c2d97f62a4f
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-05-16 23:29:40 -0400

    partially completed footer.js

---
+++ src/components/pages/Footer.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { FaLinkedin, FaGithub, FaStackOverflow } from "react-icons/fa";
+
+
+export default function Footer() {
+    return (
+        <div>
+            <footer>
+                <ul>
+                    <a href="https://github.com/tlequernaque">
+                        <li>
+                            <FaGithub/>
+                        </li> 
+                    </a>
+                    <a href="https://www.linkedin.com/">
+                        <li>
+                            <FaLinkedin/>
+                        </li>
+                    </a>
+                    <a href="https://stackoverflow.com/">
+                        <li>
+                            <FaStackOverflow/>
+                        </li>
+                    </a>
+                </ul>  
+            </footer>
+        </div>
+    );
+}
