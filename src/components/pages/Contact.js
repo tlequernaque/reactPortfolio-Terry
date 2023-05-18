a6d9ed929fdc4311aa855c5ecacff5c72caa77ac
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-05-18 17:26:14 -0400

    partially completed contact

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,22 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
+import validator from 'validator';
 
 export default function Contact() {
+  const [formData, setFormData] = useState({name: "",email: "",message: ""});
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Name: ${formData.name}, Email: ${formData.email}, Message: ${formData.message}`
+    );
+  
+  }
+
+  const [emailError, setEmailError] = useState('')
+  const validateEmail = (e) => {
+    var email = e.target.value
+  
+    if (validator.isEmail(email)) {
+      setEmailError('Valid Email :)')
+    } else {
+      setEmailError('Enter valid Email!')
+    }
+  };
   return (
     <div>
       <h1>Contact Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+      <form onSubmit = {handleSubmit}>
+      <label htmlFor="name">Name:</label>
+      <input type="text" id="name" name="name" value={formData.name} onChange={handleChange}/>
+
+      <label htmlFor="email">Email:</label>
+      <input type="email" id="email" name="email" onChange={(e) => validateEmail(e)} value={formData.email} onChange={handleChange}/>
+      <br></br>
+      <span style={{
+          fontWeight: 'bold',
+          color: 'red',
+        }}>{emailError}</span>
+
+      <label htmlFor="message">Message:</label>
+      <textarea id="message" name="message" value={formData.message} onChange={handleChange} />
+
+      <button type="submit">Submit</button>
+    </form>
     </div>
   );
-}
+};
