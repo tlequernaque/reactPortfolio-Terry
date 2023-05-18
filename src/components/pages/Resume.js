e71faae2e214db3cbc6d4da8cc3888e0e6732c29
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-05-18 01:06:40 -0400

    partially completed resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,17 +1,36 @@
 import React from 'react';
+import {FaFileDownload} from 'react-icons/fa'
+let resume = "./assets/images/resume.pdf"
 
 export default function Resume() {
   return (
     <div>
       <h1>Resume Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+      <br></br>
+      <div>
+        <h6>Download Resume</h6>
+        <a href={process.env.PUBLIC_URL+resume} target="_blank" rel="noopener noreferrer">
+          <FaFileDownload className="m-3" size={42}/>
+          <p></p>
+        </a>
+        <h4>Front-end Proficiencies</h4>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>JQuery</li>
+        <li>Responsive design</li>
+        <li>React</li>
+        <li>Bootstrap</li>
+        <br></br>
+        <h4>Back-end Proficiencies</h4>
+        <li>APIs</li>
+        <li>Node</li>
+        <li>Express</li>
+        <li>MySQL, Sequelize</li>
+        <li>MongoDB, Mongoose</li>
+        <li>REST</li>
+        <li>GraphQL</li>
+      </div>
     </div>
   );
 }
