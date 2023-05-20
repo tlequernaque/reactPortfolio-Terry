b1dfa7a88a4d9f7904fd6b0c8f2b15837a0a11bf
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-05-20 02:47:02 -0400

    updated code in portfolio

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaGithub } from "react-icons/fa";
 let Astrnomy = "./assets/images/astrnmy.png"
 let cloudCalorie = "./assets/images/cloudcalorie.png"
 
@@ -7,6 +8,38 @@ export default function Portfolio() {
     <div>
       <h1>Portfolio Page</h1>
       <div className="d-flex flex-row m-3">
+        <div className="card" style={{width: '18rem'}}>
+          <img src={process.env.PUBLIC_URL+Astrnomy}  alt="astranomy project" className="card-img-top"></img>
+          <div className="card-body">
+            <h5 className="card-title">Astrnomy</h5>
+            <p className="card-text"></p>
+            <a href='https://github.com/blairrrrwho/astrnmy.git'>
+              <FaGithub className="m-3" size={42}/>
+            </a>
+            <a href='https://blairrrrwho.github.io/astrnmy/#rm' className="btn btn-primary">URL</a>
+          </div>
+        </div>
+
+        <div className="card" style={{width: '18rem'}}>
+          <img src={process.env.PUBLIC_URL+cloudCalorie}  alt="clouCalorie project" className="card-img-top"></img>
+          <div className="card-body">
+            <h5 className="card-title">cloudCalorie </h5>
+            <p className="card-text"></p>
+            <a href='https://github.com/AaronDreyer/CloudCalorie.git'>
+              <FaGithub className="m-3" size={42}/>
+            </a>
+            <a href='https://cloudcalorie.herokuapp.com/login' className="btn btn-primary">URL</a>
+          </div>
+        </div>
+
+
+
+      </div>
+      
+
+
+
+      {/* <div className="d-flex flex-row m-3">
         <section className="m-3">
           <div className="border border-dark">
             <div >
@@ -29,7 +62,7 @@ export default function Portfolio() {
             </div>
           </div>
         </section>
-      </div>
+      </div> */}
     </div>
   );
 }
