aaf22d270d796e5bd726746caa3dcdbab1e87ad6
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-05-20 03:43:41 -0400

    partially completed portfolio

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { FaGithub } from "react-icons/fa";
-let Astrnomy = "./assets/images/astrnmy.png"
-let cloudCalorie = "./assets/images/cloudcalorie.png"
+let Astrnomy = "./assets/images/astrnmy.png";
+let cloudCalorie = "./assets/images/cloudcalorie.png";
+let myWeatherApi = "./assets/images/My-Weather-API.png";
+let ultimateNoteTaker = "./assets/images/ultimate-noteTaker.png";
+let workdayScheduler = "./assets/images/Third-party-api-work-day-scheduler_.png";
+let textEditor = "./assets/images/ultimate-texteditor-pwa.herokuapp.com_.png"
 
 export default function Portfolio() {
   return (
@@ -31,6 +35,55 @@ export default function Portfolio() {
             <a href='https://cloudcalorie.herokuapp.com/login' className="btn btn-primary">URL</a>
           </div>
         </div>
+
+        <div className="card" style={{width: '18rem'}}>
+          <img src={process.env.PUBLIC_URL+myWeatherApi}  alt="myWeather Api project" className="card-img-top" height={600}></img>
+          <div className="card-body">
+            <h5 className="card-title">Weather App </h5>
+            <p className="card-text"></p>
+            <a href='https://github.com/tlequernaque/My-Weather-API.git'>
+              <FaGithub className="m-3" size={42}/>
+            </a>
+            <a href='https://tlequernaque.github.io/My-Weather-API/' className="btn btn-primary">URL</a>
+          </div>
+        </div>
+
+        <div className="card" style={{width: '18rem'}}>
+          <img src={process.env.PUBLIC_URL+ultimateNoteTaker}  alt="noteTaker app" className="card-img-top" height={600}></img>
+          <div className="card-body">
+            <h5 className="card-title">Note Taker App </h5>
+            <p className="card-text"></p>
+            <a href='https://github.com/tlequernaque/ultimate-noteTaker.git'>
+              <FaGithub className="m-3" size={42}/>
+            </a>
+            <a href='https://incredible-note-taker-express.herokuapp.com/' className="btn btn-primary">URL</a>
+          </div>
+        </div>
+
+        <div className="card" style={{width: '18rem'}}>
+          <img src={process.env.PUBLIC_URL+workdayScheduler}  alt="workday scheduler app" className="card-img-top" height={600}></img>
+          <div className="card-body">
+            <h5 className="card-title">Workday Scheduler App </h5>
+            <p className="card-text"></p>
+            <a href='https://github.com/tlequernaque/Third-party-api-work-day-scheduler.git'>
+              <FaGithub className="m-3" size={42}/>
+            </a>
+            <a href='https://tlequernaque.github.io/Third-party-api-work-day-scheduler/' className="btn btn-primary">URL</a>
+          </div>
+        </div>
+
+        <div className="card" style={{width: '18rem'}}>
+          <img src={process.env.PUBLIC_URL+textEditor}  alt="textEditor app" className="card-img-top" height={600}></img>
+          <div className="card-body">
+            <h5 className="card-title">Text Editor App </h5>
+            <p className="card-text"></p>
+            <a href='https://github.com/tlequernaque/Ultimate-textEditor-PWA.git'>
+              <FaGithub className="m-3" size={42}/>
+            </a>
+            <a href='https://ultimate-texteditor-pwa.herokuapp.com/' className="btn btn-primary">URL</a>
+          </div>
+        </div>
+
 
 
 
