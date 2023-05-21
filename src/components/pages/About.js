088968589ef3c60761f89e07a30eb771e18c1870
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-05-21 03:29:37 -0400

    finished styling for about.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,7 +5,10 @@ export default function About() {
   return (
     <div>
       <h1>About Page</h1>
-      <img src={process.env.PUBLIC_URL+myPic} alt="my pic" height={250}></img>
+      <div className="d-flex justify-content-center">
+        <img src={process.env.PUBLIC_URL+myPic} alt="my pic" height={250}></img>
+      </div>
+      <br></br>
       <p>
       My name is Terry Lequernaque, I'm 28 and I'm from venezuela. I've lived in Georgia most of my life. I grew up in Atlanta but I moved to Savannah 5 years ago. I recently starter attending a coding bootcamp at GATech. This is my portfolio of different work projects that I have completed.
       </p>
