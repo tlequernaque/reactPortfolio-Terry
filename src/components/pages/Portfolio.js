6f11239100ea60da5e03d9f19b24e6d5a3e6722c
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-05-21 03:18:56 -0400

    upgraded code efor portfolio.js and added images

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FaGithub } from "react-icons/fa";
-let Astrnomy = "./assets/images/astrnmy.png";
-let cloudCalorie = "./assets/images/cloudcalorie.png";
+let Astrnomy = "./assets/images/astrnmy2.png";
+let cloudCalorie = "./assets/images/cloudcalorie2.png";
 let myWeatherApi = "./assets/images/My-Weather-API.png";
 let ultimateNoteTaker = "./assets/images/ultimate-noteTaker.png";
 let workdayScheduler = "./assets/images/Third-party-api-work-day-scheduler_.png";
@@ -11,76 +11,93 @@ export default function Portfolio() {
   return (
     <div>
       <h1>Portfolio Page</h1>
-      <div className="d-flex justify-content-evenly m-3">
-        <div className="card" style={{width: '18rem'}}>
-          <img src={process.env.PUBLIC_URL+Astrnomy}  alt="astranomy project" className="card-img-top" height={600}></img>
+      <div className=" d-flex flex-wrap m-3">
+        <div className="card m-4" style={{width: '18rem'}}>
+          <img src={process.env.PUBLIC_URL+Astrnomy}  alt="astranomy project" className="card-img-top" height={200}></img>
           <div className="card-body">
-            <h5 className="card-title">Astrnomy</h5>
+            <h5 className=" mx-auto card-title"> Astrnomy</h5>
             <p className="card-text"></p>
-            <a href='https://github.com/blairrrrwho/astrnmy.git'>
-              <FaGithub className="m-3" size={42}/>
-            </a>
-            <a href='https://blairrrrwho.github.io/astrnmy/#rm' className="btn btn-primary">URL</a>
+            <div className="d-flex justify-content-center">
+              <a className='' href='https://github.com/blairrrrwho/astrnmy.git'>
+              <FaGithub className="me-3" size={42}/>
+              </a>
+              <a href='https://blairrrrwho.github.io/astrnmy/#rm' className="btn btn-primary">URL</a>
+            </div>
+            
           </div>
         </div>
 
-        <div className="card" style={{width: '18rem'}}>
-          <img src={process.env.PUBLIC_URL+cloudCalorie}  alt="clouCalorie project" className="card-img-top" height={600}></img>
+        <div className="card m-4" style={{width: '18rem'}}>
+          <img src={process.env.PUBLIC_URL+cloudCalorie}  alt="clouCalorie project" className="card-img-top" height={200}></img>
           <div className="card-body">
             <h5 className="card-title">CloudCalorie </h5>
             <p className="card-text"></p>
-            <a href='https://github.com/AaronDreyer/CloudCalorie.git'>
-              <FaGithub className="m-3" size={42}/>
-            </a>
-            <a href='https://cloudcalorie.herokuapp.com/login' className="btn btn-primary">URL</a>
+            <div className="d-flex justify-content-center">
+              <a href='https://github.com/AaronDreyer/CloudCalorie.git'>
+              <FaGithub className="me-3" size={42}/>
+              </a>
+              <a href='https://cloudcalorie.herokuapp.com/login' className="btn btn-primary">URL</a>
+            </div>
+            
           </div>
         </div>
 
-        <div className="card" style={{width: '18rem'}}>
-          <img src={process.env.PUBLIC_URL+myWeatherApi}  alt="myWeather Api project" className="card-img-top" height={600}></img>
+        <div className="card m-4" style={{width: '18rem'}}>
+          <img src={process.env.PUBLIC_URL+myWeatherApi}  alt="myWeather Api project" className="card-img-top" height={200}></img>
           <div className="card-body">
             <h5 className="card-title">Weather App </h5>
             <p className="card-text"></p>
-            <a href='https://github.com/tlequernaque/My-Weather-API.git'>
-              <FaGithub className="m-3" size={42}/>
-            </a>
-            <a href='https://tlequernaque.github.io/My-Weather-API/' className="btn btn-primary">URL</a>
+            <div className="d-flex justify-content-center">
+              <a href='https://github.com/tlequernaque/My-Weather-API.git'>
+              <FaGithub className="me-3" size={42}/>
+              </a>
+              <a href='https://tlequernaque.github.io/My-Weather-API/' className="btn btn-primary">URL</a>
+            </div>
           </div>
         </div>
 
-        <div className="card" style={{width: '18rem'}}>
-          <img src={process.env.PUBLIC_URL+ultimateNoteTaker}  alt="noteTaker app" className="card-img-top" height={600}></img>
+        <div className="card m-4" style={{width: '18rem'}}>
+          <img src={process.env.PUBLIC_URL+ultimateNoteTaker}  alt="noteTaker app" className="card-img-top" height={200}></img>
           <div className="card-body">
             <h5 className="card-title">Note Taker App </h5>
             <p className="card-text"></p>
-            <a href='https://github.com/tlequernaque/ultimate-noteTaker.git'>
-              <FaGithub className="m-3" size={42}/>
-            </a>
-            <a href='https://incredible-note-taker-express.herokuapp.com/' className="btn btn-primary">URL</a>
+            <div className="d-flex justify-content-center">
+              <a href='https://github.com/tlequernaque/ultimate-noteTaker.git'>
+              <FaGithub className="me-3" size={42}/>
+              </a>
+              <a href='https://incredible-note-taker-express.herokuapp.com/' className="btn btn-primary">URL</a>
+            </div>
+            
           </div>
         </div>
 
-        <div className="card" style={{width: '18rem'}}>
-          <img src={process.env.PUBLIC_URL+workdayScheduler}  alt="workday scheduler app" className="card-img-top" height={600}></img>
+        <div className="card m-4" style={{width: '18rem'}}>
+          <img src={process.env.PUBLIC_URL+workdayScheduler}  alt="workday scheduler app" className="card-img-top" height={200}></img>
           <div className="card-body">
             <h5 className="card-title">Workday Scheduler App </h5>
             <p className="card-text"></p>
-            <a href='https://github.com/tlequernaque/Third-party-api-work-day-scheduler.git'>
-              <FaGithub className="m-3" size={42}/>
-            </a>
-            <a href='https://tlequernaque.github.io/Third-party-api-work-day-scheduler/' className="btn btn-primary">URL</a>
+            <div className="d-flex justify-content-center">
+              <a href='https://github.com/tlequernaque/Third-party-api-work-day-scheduler.git'>
+              <FaGithub className="me-3" size={42}/>
+              </a>
+              <a href='https://tlequernaque.github.io/Third-party-api-work-day-scheduler/' className="btn btn-primary">URL</a>
+            </div>
+            
           </div>
         </div>
 
-        <div className="card" style={{width: '18rem'}}>
-          <img src={process.env.PUBLIC_URL+textEditor}  alt="textEditor app" className="card-img-top" height={600}></img>
+        <div className="card m-4" style={{width: '18rem'}}>
+          <img src={process.env.PUBLIC_URL+textEditor}  alt="textEditor app" className="card-img-top" height={200}></img>
           <div className="card-body">
             <h5 className="card-title">Text Editor App </h5>
             <p className="card-text"></p>
-            <a href='https://github.com/tlequernaque/Ultimate-textEditor-PWA.git'>
-              <FaGithub className="m-3" size={42}/>
-            </a>
-            <a href='https://ultimate-texteditor-pwa.herokuapp.com/' className="btn btn-primary">URL</a>
+            <div className="d-flex justify-content-center">
+              <a href='https://github.com/tlequernaque/Ultimate-textEditor-PWA.git'>
+              <FaGithub className="me-3" size={42}/>
+              </a>
+              <a href='https://ultimate-texteditor-pwa.herokuapp.com/' className="btn btn-primary">URL</a>
+            </div>
+            
           </div>
         </div>
       </div>
